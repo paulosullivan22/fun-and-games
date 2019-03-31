2c9b501ecd92a00b41b6419ee624edd3e7b8cc82
paul <paulosullivan22@outlook.com>
2019-03-31 20:27:37 +0100

    js clock

--- javascript30/02 - CSS & JS Clock/script.js
+++ javascript30/02 - CSS & JS Clock/script.js
@@ -1,9 +1,19 @@
 var d = new Date();
 
-console.log(d.getHours());
-console.log(d.getMinutes());
-console.log(d.getSeconds());
-
+let hourAngle = d.getHours() * 15;
 let hourHand = document.getElementById('hour-hand');
 
-hourHand.add
+function handRotate(hand, angle) {
+  console.log(angle)
+  hand.style.transform = `rotate(${angle}deg)`;
+}
+
+handRotate(hourHand, hourAngle);
+
+// Method
+//
+// Find the hour, mins and seconds
+//
+// Set each hand to the correct time
+//
+// set a timeout
